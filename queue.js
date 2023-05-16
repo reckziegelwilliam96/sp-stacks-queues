d959af38a5404f8b66e136be4cc6b06763a1ce58
Liam Reckziegel <liamreckziegel@Liams-iMac.local>
2023-05-16 13:32:36 -0700

    commit completed files

---
+++ queue.js
@@ -0,0 +1,55 @@
+/** Node: node for a queue. */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/** Queue: chained-together nodes where you can
+ *  remove from the front or add to the back. */
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+
+  dequeue() {
+    if (!this.first) throw new Error("Queue is empty");
+
+    const temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.val;
+  }
+
+  peek() {
+    if (!this.first) throw new Error("Queue is empty");
+    return this.first.val;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+}
+
+module.exports = Queue;
+
---
+++ stack.js
@@ -0,0 +1,56 @@
+/** Node: node for a stack. */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/** Stack: chained-together nodes where you can
+ *  remove from the top or add to the top. */
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      const temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+
+  pop() {
+    if (this.size === 0) throw new Error("Stack is empty");
+
+    const temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.val;
+  }
+
+  peek() {
+    if (this.size === 0) throw new Error("Stack is empty");
+    return this.first.val;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+}
+
+
+module.exports = Stack;
